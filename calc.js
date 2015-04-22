75bb1f70711f72d588711cecd695dab294be25b5
longbeardmen <longbeardmen@gmail.com>
2015-04-22 04:16:11 +0300

    add some fiatures

--- calc.js
+++ calc.js
@@ -4,28 +4,26 @@
 
 window.onload = function (button) {
 
-	var tr = document.getElementsByClassName('tr');
+	var tr = document.getElementsByClassName('tr'),
+		plus = document.getElementById('plus'),
+		minus = document.getElementById('minus'),
+		multiply = document.getElementById('multiply'),
+		divide = document.getElementById('divide'),
+		power = document.getElementById('power'),
+		reset = document.getElementById('fresh'),
+		answr = document.getElementById('answr');
+
 	for(var i = 0; i < tr.length; i++) {
 		tr[i].addEventListener('click', calculatorBrains, false);
 	}
 
-	var plus = document.getElementById('plus');
-		plus.addEventListener('click', calculatorBrains, false);
-
-	var minus = document.getElementById('minus');
-		minus.addEventListener('click', calculatorBrains, false);
-
-	var multiply = document.getElementById('multiply');
-		multiply.addEventListener('click', calculatorBrains, false);
-
-	var divide = document.getElementById('divide');
-		divide.addEventListener('click', calculatorBrains, false);
-			
-	var reset = document.getElementById('fresh');
-		reset.addEventListener('click', calculatorBrains, false);
-
-	var answr = document.getElementById('answr');
-		answr.addEventListener('click', calculatorBrains, false);
+	plus.addEventListener('click', calculatorBrains, false);
+	minus.addEventListener('click', calculatorBrains, false);
+	multiply.addEventListener('click', calculatorBrains, false);
+	divide.addEventListener('click', calculatorBrains, false);
+	power.addEventListener('click', calculatorBrains, false);
+	reset.addEventListener('click', calculatorBrains, false);
+	answr.addEventListener('click', calculatorBrains, false);
 };
 
 /*
@@ -40,8 +38,8 @@ window.onload = function (button) {
 var a = '',
 	b = '',
 	cur = '',
-	curElement,
-	input;
+	cPower = false;
+	
 
 /*
 	define calculatorBrains()
@@ -55,254 +53,275 @@ function calculatorBrains (button) {
 
 	input = document.getElementById('i');
 	curElement = button.target;
-	
+	console.log(curElement.firstChild.nodeValue);
+
 	/*
 		cheсk entered simbol
 	*/
 
-	if(/^\d+$/.test(curElement.firstChild.nodeValue)){
-		input.value += curElement.firstChild.nodeValue;
-		if(cur === ''){
-			a = input.value;
-		}
-		if(cur !== '' && a !== ''){
-			b += curElement.firstChild.nodeValue;
-		}
-	}else if(a !== '' && curElement.firstChild.nodeValue == '+'){
-		console.log(cur);
-		if(b !== ''){
-			switch(cur){
-				case 'plus':
-					input.value = '';
-					input.value += 1*a + 1*b;
-					a = input.value;
-					b = '';
-					input.value += curElement.firstChild.nodeValue;
-					break;
-				case 'minus':
-					input.value = '';
-					input.value += 1*a - 1*b;
-					a = input.value;
-					b = '';
-					input.value += curElement.firstChild.nodeValue;
-					break;
-				case 'multiply':
-					input.value = '';
-					input.value += (1*a) * (1*b);
-					a = input.value;
-					b = '';
-					input.value += curElement.firstChild.nodeValue;
-					break;
-				case 'divide':
-					if(1*b !== 0){
-						input.value = '';
-						input.value += (1*a) / (1*b);
-						a = input.value;
-						b = '';
-						input.value += curElement.firstChild.nodeValue;
-					}else{
-						input.value = '';
-						a = '';
-						b = '';
-						confirm('illegal dividing by 0!!!');
-					}
-					break;
-			}
+	if(curElement.firstChild.nodeValue === 'ON/OFF'){
+		if(cPower){
+			cPower = false;
+			console.log('calculator is OFF');
+			input.value = 'Run, nigga!!! Run!!!';
+			setTimeout(function (){
+				input.value = '';
+				}, 1500);
 		}else{
-			input.value += curElement.firstChild.nodeValue;
-		}
-		if(input.value !== '')
-			cur = 'plus';
-		else
-			cur='';
-	}else if(a !== '' && curElement.firstChild.nodeValue == '-'){
-		console.log(cur);
-		if(b !== ''){
-			switch(cur){
-				case 'plus':
-					input.value = '';
-					input.value += 1*a + 1*b;
-					a = input.value;
-					b = '';
-					input.value += curElement.firstChild.nodeValue;
-					break;
-				case 'minus':
-					input.value = '';
-					input.value += 1*a - 1*b;
-					a = input.value;
-					b = '';
-					input.value += curElement.firstChild.nodeValue;
-					break;
-				case 'multiply':
-					input.value = '';
-					input.value += (1*a) * (1*b);
-					a = input.value;
-					b = '';
-					input.value += curElement.firstChild.nodeValue;
-					break;
-				case 'divide':
-					if(1*b !== 0){
-						input.value = '';
-						input.value += (1*a) / (1*b);
-						a = input.value;
-						b = '';
-						input.value += curElement.firstChild.nodeValue;
-					}else{
-						input.value = '';
-						a = '';
-						b = '';
-						confirm('illegal dividing by 0!!!');
-					}
-					break;
-			}
-		}else{
-			input.value += curElement.firstChild.nodeValue;
-		}
-		if(input.value !== '')
-			cur = 'minus';
-		else
-			cur='';
-	}else if(curElement.firstChild.nodeValue == '*'){
-		console.log(cur);
-		if(b !== ''){
-			switch(cur){
-				case 'plus':
-					input.value = '';
-					input.value += 1*a + 1*b;
-					a = input.value;
-					b = '';
-					input.value += curElement.firstChild.nodeValue;
-					break;
-				case 'minus':
-					input.value = '';
-					input.value += 1*a - 1*b;
-					a = input.value;
-					b = '';
-					input.value += curElement.firstChild.nodeValue;
-					break;
-				case 'multiply':
-					input.value = '';
-					input.value += (1*a) * (1*b);
-					a = input.value;
-					b = '';
-					input.value += curElement.firstChild.nodeValue;
-					break;
-				case 'divide':
-					if(1*b !== 0){
-						input.value = '';
-						input.value += (1*a) / (1*b);
-						a = input.value;
-						b = '';
-						input.value += curElement.firstChild.nodeValue;
-					}else{
-						input.value = '';
-						a = '';
-						b = '';
-						confirm('illegal dividing by 0!!!');
-					}
-					break;
-			}
-		}else
-			input.value += curElement.firstChild.nodeValue;
-		if(input.value !== '')
-			cur = 'multiply';
-		else
-			cur='';
-	}else if(curElement.firstChild.nodeValue == '/'){
-		console.log(cur);
-		if(b !==''){
-			switch(cur){
-				case 'plus':
-					input.value = '';
-					input.value += 1*a + 1*b;
-					a = input.value;
-					b = '';
-					input.value += curElement.firstChild.nodeValue;
-					break;
-				case 'minus':
-					input.value = '';
-					input.value += 1*a - 1*b;
-					a = input.value;
-					b = '';
-					input.value += curElement.firstChild.nodeValue;
-					break;
-				case 'multiply':
-					input.value = '';
-					input.value += (1*a) * (1*b);
-					a = input.value;
-					b = '';
-					input.value += curElement.firstChild.nodeValue;
-					break;
-				case 'divide':
-					if(1*b !== 0){
-						input.value = '';
-						input.value += (1*a) / (1*b);
-						a = input.value;
-						b = '';
-						input.value += curElement.firstChild.nodeValue;
-					}else{
-						input.value = '';
-						a = '';
-						b = '';
-						confirm('illegal dividing by 0!!!');
-					}
-					break;
-			}
-		}
-		else
-			input.value += curElement.firstChild.nodeValue;
-		if(input.value !== '')
-			cur = 'divide';
-		else
-			cur='';
-	}else if(curElement.firstChild.nodeValue == '='){
-		console.log(cur);
-		switch(cur){
-			case 'plus':
+			cPower = true;
+			console.log('calculator is ON.');
+			input.value = 'Do math, nigga!!!';
+			setTimeout(function (){
 				input.value = '';
-				input.value += 1*a + 1*b;
-				a = input.value;
-				b = '';
-				cur = '';
-				break;
-			case 'minus':
-				input.value = '';
-				input.value += 1*a - 1*b;
-				a = input.value;
-				b = '';
-				cur = '';
-				break;
-			case 'multiply':
-				input.value = '';
-				input.value += (1*a) * (1*b);
-				a = input.value;
-				b = '';
-				cur = '';
-				break;
-			case 'divide':
-				if(1*b !== 0){
-					input.value = '';
-					input.value += (1*a) / (1*b);
+				}, 1500);
+		}
+	}else{
+		if(cPower){
+			if(/^\d+$/.test(curElement.firstChild.nodeValue)){
+				input.value += curElement.firstChild.nodeValue;
+				if(cur === ''){
 					a = input.value;
-					cur = '';
-					b = '';
-				}else{
-					cur = '';
-					input.value = '';
-					a = '';
-					b = '';
-					cur = '';
-					confirm('illegal dividing by 0!!!');
 				}
-				break;
+				if(cur !== '' && a !== ''){
+					b += curElement.firstChild.nodeValue;
+				}
+			}else if(a !== '' && curElement.firstChild.nodeValue == '+'){
+				console.log(cur);
+				if(b !== ''){
+					switch(cur){
+						case 'plus':
+							input.value = '';
+							input.value += 1*a + 1*b;
+							a = input.value;
+							b = '';
+							input.value += curElement.firstChild.nodeValue;
+							break;
+						case 'minus':
+							input.value = '';
+							input.value += 1*a - 1*b;
+							a = input.value;
+							b = '';
+							input.value += curElement.firstChild.nodeValue;
+							break;
+						case 'multiply':
+							input.value = '';
+							input.value += (1*a) * (1*b);
+							a = input.value;
+							b = '';
+							input.value += curElement.firstChild.nodeValue;
+							break;
+						case 'divide':
+							if(1*b !== 0){
+								input.value = '';
+								input.value += (1*a) / (1*b);
+								a = input.value;
+								b = '';
+								input.value += curElement.firstChild.nodeValue;
+							}else{
+								input.value = '';
+								a = '';
+								b = '';
+								confirm('illegal dividing by 0!!!');
+							}
+							break;
+					}
+				}else{
+					input.value += curElement.firstChild.nodeValue;
+				}
+				if(input.value !== '')
+					cur = 'plus';
+				else
+					cur='';
+			}else if(a !== '' && curElement.firstChild.nodeValue == '-'){
+				console.log(cur);
+				if(b !== ''){
+					switch(cur){
+						case 'plus':
+							input.value = '';
+							input.value += 1*a + 1*b;
+							a = input.value;
+							b = '';
+							input.value += curElement.firstChild.nodeValue;
+							break;
+						case 'minus':
+							input.value = '';
+							input.value += 1*a - 1*b;
+							a = input.value;
+							b = '';
+							input.value += curElement.firstChild.nodeValue;
+							break;
+						case 'multiply':
+							input.value = '';
+							input.value += (1*a) * (1*b);
+							a = input.value;
+							b = '';
+							input.value += curElement.firstChild.nodeValue;
+							break;
+						case 'divide':
+							if(1*b !== 0){
+								input.value = '';
+								input.value += (1*a) / (1*b);
+								a = input.value;
+								b = '';
+								input.value += curElement.firstChild.nodeValue;
+							}else{
+								input.value = '';
+								a = '';
+								b = '';
+								confirm('illegal dividing by 0!!!');
+							}
+							break;
+					}
+				}else{
+					input.value += curElement.firstChild.nodeValue;
+				}
+				if(input.value !== '')
+					cur = 'minus';
+				else
+					cur='';
+			}else if(curElement.firstChild.nodeValue == '*'){
+				console.log(cur);
+				if(b !== ''){
+					switch(cur){
+						case 'plus':
+							input.value = '';
+							input.value += 1*a + 1*b;
+							a = input.value;
+							b = '';
+							input.value += curElement.firstChild.nodeValue;
+							break;
+						case 'minus':
+							input.value = '';
+							input.value += 1*a - 1*b;
+							a = input.value;
+							b = '';
+							input.value += curElement.firstChild.nodeValue;
+							break;
+						case 'multiply':
+							input.value = '';
+							input.value += (1*a) * (1*b);
+							a = input.value;
+							b = '';
+							input.value += curElement.firstChild.nodeValue;
+							break;
+						case 'divide':
+							if(1*b !== 0){
+								input.value = '';
+								input.value += (1*a) / (1*b);
+								a = input.value;
+								b = '';
+								input.value += curElement.firstChild.nodeValue;
+							}else{
+								input.value = '';
+								a = '';
+								b = '';
+								confirm('illegal dividing by 0!!!');
+							}
+							break;
+					}
+				}else
+					input.value += curElement.firstChild.nodeValue;
+				if(input.value !== '')
+					cur = 'multiply';
+				else
+					cur='';
+			}else if(curElement.firstChild.nodeValue == '/'){
+				console.log(cur);
+				if(b !==''){
+					switch(cur){
+						case 'plus':
+							input.value = '';
+							input.value += 1*a + 1*b;
+							a = input.value;
+							b = '';
+							input.value += curElement.firstChild.nodeValue;
+							break;
+						case 'minus':
+							input.value = '';
+							input.value += 1*a - 1*b;
+							a = input.value;
+							b = '';
+							input.value += curElement.firstChild.nodeValue;
+							break;
+						case 'multiply':
+							input.value = '';
+							input.value += (1*a) * (1*b);
+							a = input.value;
+							b = '';
+							input.value += curElement.firstChild.nodeValue;
+							break;
+						case 'divide':
+							if(1*b !== 0){
+								input.value = '';
+								input.value += (1*a) / (1*b);
+								a = input.value;
+								b = '';
+								input.value += curElement.firstChild.nodeValue;
+							}else{
+								input.value = '';
+								a = '';
+								b = '';
+								confirm('illegal dividing by 0!!!');
+							}
+							break;
+					}
+				}
+				else
+					input.value += curElement.firstChild.nodeValue;
+				if(input.value !== '')
+					cur = 'divide';
+				else
+					cur='';
+			}else if(curElement.firstChild.nodeValue == '='){
+				console.log(cur);
+				switch(cur){
+					case 'plus':
+						input.value = '';
+						input.value += 1*a + 1*b;
+						a = input.value;
+						b = '';
+						cur = '';
+						break;
+					case 'minus':
+						input.value = '';
+						input.value += 1*a - 1*b;
+						a = input.value;
+						b = '';
+						cur = '';
+						break;
+					case 'multiply':
+						input.value = '';
+						input.value += (1*a) * (1*b);
+						a = input.value;
+						b = '';
+						cur = '';
+						break;
+					case 'divide':
+						if(1*b !== 0){
+							input.value = '';
+							input.value += (1*a) / (1*b);
+							a = input.value;
+							cur = '';
+							b = '';
+						}else{
+							cur = '';
+							input.value = '';
+							a = '';
+							b = '';
+							cur = '';
+							confirm('illegal dividing by 0!!!');
+						}
+						break;
+				}
+			}else if(curElement.firstChild.nodeValue == 'reset'){
+				input.value = '';
+				a = '';
+				b = '';
+				cur = '';
+			}
+			console.log(a);
+			console.log(b);
+			console.log(cur);
 		}
-	}else if(curElement.firstChild.nodeValue == 'reset'){
-		input.value = '';
-		a = '';
-		b = '';
-		cur = '';
 	}
-	console.log(a);
-	console.log(b);
-	console.log(cur);
 }
